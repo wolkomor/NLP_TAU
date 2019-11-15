2f9dfe5cf3e314d6bbc3609908379a163fd8bfcc
wolkomor <wolkomor@gmail.com>
2019-11-15 18:48:27 +0200

    sgd

--- HW1/Q3/q3d_sgd.py
+++ HW1/Q3/q3d_sgd.py
@@ -87,7 +87,8 @@ def sgd(f, x0, step, iterations, postprocessing=None, use_saved=False,
 
         loss = None
         ### YOUR CODE HERE
-        raise NotImplementedError
+        loss, grad = f(x)
+        x -= step*grad
         ### END YOUR CODE
 
         x = postprocessing(x)
